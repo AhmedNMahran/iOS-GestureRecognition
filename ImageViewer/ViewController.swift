3e36720967bbb6a2d581d257ce14ca940c73aed3
AhmedNMahran <ahmed.mdeveloper@gmail.com>
2021-12-27 09:57:43 +0200

    gesture recognition with ImageView sample

--- ImageViewer/ViewController.swift
+++ ImageViewer/ViewController.swift
@@ -9,11 +9,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func handlePan(_ sender: UIPanGestureRecognizer) {
+        let translation = sender.translation(in: view)
+        imageView.center = CGPoint(x: translation.x + imageView.center.x, y: translation.y + imageView.center.y)
+        sender.setTranslation(CGPoint.zero, in: self.view)
+    }
+    
+    @IBAction func handlePinch(_ sender: UIPinchGestureRecognizer) {
+        imageView.transform = imageView.transform.scaledBy(x: sender.scale, y: sender.scale)
+        sender.scale = 1
+    }
+    
+    @IBAction func handleRotation(_ sender: UIRotationGestureRecognizer) {
+        imageView.transform = imageView.transform.rotated(by: sender.rotation)
+        sender.rotation = 0
+    }
+}
 
+extension ViewController : UIGestureRecognizerDelegate{
+    func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer) -> Bool {
+        true
+    }
+    
 }
 
